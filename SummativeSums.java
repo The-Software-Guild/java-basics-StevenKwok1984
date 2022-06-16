74ae34f0dfdfdf0faecbad035ae7a8cf88ec8bc0
Steven Kwok <derekkwok2020@gmail.com>
2022-06-16 14:05:20 +0100

    Update SummativeSums.java
    All commends are finished

--- SummativeSums.java
+++ SummativeSums.java
@@ -1,33 +1,41 @@
 /**
- * This is my answer of Summative Sums Exercise
+ * Assessment Question: Summative Sums
+ * Steven Kwok
  */
 
 public class SummativeSums {
-
+    // main method
     public static void main(String[] args) {
-        // Assign result
-        int[][] intArray = {{ 1, 90, -33, -55, 67, -16, 28, -55, 15 },
-                { 999, -60, -77, 14, 160, 301 }, { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
-                140, 150, 160, 170, 180, 190, 200, -99 } };
 
+        // Set up all three arrays according to the question
+        int[] Array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+        int[] Array2 = { 999, -60, -77, 14, 160, 301 };
+        int[] Array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+                140, 150, 160, 170, 180, 190, 200, -99 };
+
+        // Combine all three arrays into one
+        int[][] combineArrays = { Array1, Array2, Array3 };
+
+        // Print the heading
         System.out.println("--- exec-maven-plugin:1.2.1:exec (default-cli) @ Unit1 ---");
-        for (int i = 0; i < intArray.length; i++) {
-            // sum element in each array
-            int sum = 0;
-            sum = sumResult(intArray, i);
+
+        // Assign the combined array into a loop, for individual summing
+        for (int i = 0; i < combineArrays.length; i++) {
+            // Obtain sum elements in each array
+            int sum = sumResult(combineArrays, i);
             System.out.println("#" + i+1 + " Array Sum: " + sum + "");
         }
     }
 
-    // Function for return total number of each array
+    // MEthod for return total number of each array
     public static int sumResult(int[][] Array, int i) {
-
+        // initialise the total value
         int total = 0;
+        // summing all elements inside the array
         for (int j = 0; j < Array[i].length; j++) {
             total += Array[i][j];
         }
+        // return the total value
         return total;
-
     }
-
 }
